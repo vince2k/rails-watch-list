c3a05c50b961806e4b7c0842c7c94e913554e8e5
Vincent de Cabissole <vince2k@protonmail.com>
2025-02-14 09:53:32 +0100

    list controller, missing edit & update

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,46 @@
+class ListsController < ApplicationController
+  before_action :set_list , only: %i[ show edit update ]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to @list, notice: "List was successfully created." }
+        format.json { render :show, status: :created, location: @list }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,7 @@
 class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list, message: "a déjà été ajouté à cette liste" }
 end
